f61ac877884147cc31ecadbaadf3f30b2872e0aa
Rody Bertolini <pabe9903@colorado.edu>
2024-07-29 17:07:36 +0000

    fixed another syntax error

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def db_test():
 def hello_world():
     return 'Hello, World! from Rody Bertolini in 3308'
 @app.route('/db_create')
-    def db_create():
-        conn = psycopg2.connect("postgresql://rody:CAwpXsTvoU1FpEhTg4TIYZyuqD1IBXky@dpg-cqjqlqmehbks73cgrjo0-a/rody")
-        cur = conn.cursor()
-        cur.execute('''
+def db_create():
+    conn = psycopg2.connect("postgresql://rody:CAwpXsTvoU1FpEhTg4TIYZyuqD1IBXky@dpg-cqjqlqmehbks73cgrjo0-a/rody")
+    cur = conn.cursor()
+    cur.execute('''
         CREATE TABLE IF NOT EXISTS Basketball(
             First varchar(255),
             Last varchar(255),
@@ -24,7 +24,7 @@ def hello_world():
             Number int
             );
             ''')
-        conn.commit()
-        conn.close()
-        return "Basketball table created!"
+    conn.commit()
+    conn.close()
+    return "Basketball table created!"
     
