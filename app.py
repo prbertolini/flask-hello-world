ca3b861034a045de4fe6ebf6b3bcf7b61bfda26b
Rody Bertolini <pabe9903@colorado.edu>
2024-07-29 16:41:24 +0000

    added db_test function connecting to database

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://rody:CAwpXsTvoU1FpEhTg4TIYZyuqD1IBXky@dpg-cqjqlqmehbks73cgrjo0-a/rody")
+    conn.close()
+    return "DB Connection Successful!"
 @app.route('/')
 def hello_world():
-    return 'Hello, World From Rody Bertolini 3308!'
+    return 'Hello, World! from Rody Bertolini in 3308'
